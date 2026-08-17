d5ab259e0bda03502358a5bac5486475233a960f
adam0012af-ai <adam0012af@gmail.com>
2026-08-17 06:28:28 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,67 +1,28 @@
-import java.util.Properties
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-val releaseKeystoreProperties = Properties()
-val releaseKeystoreFile = rootProject.file("key.properties")
-
-if (releaseKeystoreFile.exists()) {
-    releaseKeystoreFile.inputStream().use {
-        releaseKeystoreProperties.load(it)
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-android {
-    namespace = "com.zenqivo.player"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+rootProject.layout.buildDirectory.value(newBuildDir)
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+subprojects {
+    val newSubprojectBuildDir: Directory =
+        newBuildDir.dir(project.name)
 
-    defaultConfig {
-        applicationId = "com.zenqivo.player"
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    signingConfigs {
-        if (releaseKeystoreFile.exists()) {
-            create("release") {
-                keyAlias = releaseKeystoreProperties.getProperty("keyAlias")
-                keyPassword = releaseKeystoreProperties.getProperty("keyPassword")
-                storeFile = file(
-                    releaseKeystoreProperties.getProperty("storeFile")
-                )
-                storePassword =
-                    releaseKeystoreProperties.getProperty("storePassword")
-            }
-        }
-    }
-
-    buildTypes {
-        release {
-            if (releaseKeystoreFile.exists()) {
-                signingConfig = signingConfigs.getByName("release")
-            }
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
-    }
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
-flutter {
-    source = "../.."
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
